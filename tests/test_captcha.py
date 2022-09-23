64b6b1d46f79d890864a6f31b60d7ad88014cc91
Terry <17601306065@163.com>
2022-09-23 10:58:37 +0800

    test: revise captcha service test

--- tests/test_captcha.py
+++ tests/test_captcha.py
@@ -16,18 +16,21 @@ from crawlerstack_anticaptcha.utils.upload_file import UploadedFile
     [
         'image',
         'test',
-        # 'error'
+        'error'
     ]
 )
 @pytest.mark.asyncio
 async def test_check(mocker, file_type):
     """test check"""
+    mocker.patch.object(
+        CategoryRepository,
+        'get_by_id',
+        return_value=mocker.MagicMock(type='SliderCaptcha')
+    )
     if 'image' in file_type:
-        category_repository = CategoryRepository()
         parse = mocker.patch.object(SliderCaptcha, 'parse')
         save = mocker.patch.object(UploadedFile, 'save')
         write_to_db = mocker.patch.object(CaptchaService, 'write_to_db')
-        category_repository.get_by_id = mocker.MagicMock(return_value='SliderCaptcha')
         test_file = mocker.MagicMock(
             content_type=file_type,
             file_name='foo.jpg'
@@ -50,6 +53,17 @@ async def test_check(mocker, file_type):
         captcha_service = CaptchaService(test_file, mocker.MagicMock(), data)
         result = await captcha_service.check()
         assert result.code == 415
+    if 'error' in file_type:
+        category_repository = CategoryRepository()
+        category_repository.get_by_id = mocker.MagicMock(return_value='SliderCaptcha')
+        test_file = mocker.MagicMock(
+            content_type='image',
+            file_name='foo.jpg'
+        )
+        data = mocker.MagicMock()
+        captcha_service = CaptchaService(test_file, mocker.MagicMock(), data)
+        result = await captcha_service.check()
+        assert result.message == 'ModuleNotFoundError'
 
 
 @pytest.mark.asyncio
