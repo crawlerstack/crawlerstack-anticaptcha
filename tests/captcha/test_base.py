31781fd4617c676101fc704963b37c9b32a8fc77
Terry <17601306065@163.com>
2022-11-11 13:37:42 +0800

    test: test coverage captcha factory

--- tests/captcha/test_base.py
+++ tests/captcha/test_base.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+from crawlerstack_anticaptcha.captcha import NumericalCaptcha, captcha_factory
 from crawlerstack_anticaptcha.captcha.base import BaseCaptcha
 
 
@@ -10,3 +11,10 @@ def test_parse(mock_path):
     base = BaseCaptcha(mock_path / 'foo')
     with pytest.raises(NotImplementedError):
         base.parse()
+
+
+def test_captcha_factory(mocker, mock_path):
+    """test captcha factory"""
+    mocker.patch.object(NumericalCaptcha, 'parse', return_value='foo')
+    res = captcha_factory('NumericalCaptcha', mock_path / 'foo')
+    assert res == 'foo'
